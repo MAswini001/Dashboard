42eb041f3427e1b4ea43a39ddfede3cc8cf0975f
Aswini.M <Aswini.m@iinterchange.net>
2022-04-21 17:06:05 +0530

    4th video pactice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserViewComponent } from './user-view/user-view.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopBarComponent,
+    DashboardComponent,
+    UserListComponent,
+    UserCreateComponent,
+    UserViewComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
